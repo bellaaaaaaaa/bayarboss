25061ec55343f9dfb9528daf75a1ed6ef37029df
sakowan <gbayabos21@gmail.com>
2018-11-30 19:40:14 +0800

    finished sessions for signup, login and logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+    def create
+        @user = User.new(user_params)
+        @user.save
+        log_in @user
+        redirect_to root_url
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password_digest, :password, :age)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2018_11_30_085610) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.integer "age"
     t.string "image"
     t.datetime "created_at", null: false
